f86a8eb9c80ea9860c8182401dddbb89384569ab
Your Name <you@example.com>
2024-06-04 09:07:00 +0300

    "HW4 is completed"

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,17 +9,47 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
-	queue    List
+	queue    list
 	items    map[Key]*ListItem
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
-		queue:    NewList(),
+		queue:    list{},
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	if item, ok := c.items[key]; ok {
+		item.Value = value
+		c.queue.MoveToFront(item)
+		return true
+	}
+
+	if c.queue.Len() == c.capacity {
+		delete(c.items, c.queue.Back().Value.(Key))
+		c.queue.Remove(c.queue.Back())
+	}
+
+	newItem := c.queue.PushFront(key)
+	newItem.Value = value
+	c.items[key] = newItem
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		return item.Value, true
+	}
+
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = list{}
+	c.items = make(map[Key]*ListItem)
 }
